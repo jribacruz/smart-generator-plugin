f68a875e26d5857372a02c682bb2967f76668dda
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-06-15 11:17:27 -0300

    alteracao do tipo de retorno dos metodos getValue

--- smart-generator-plugin-model/src/smart/generator/plugin/model/readers/IAnnotationReader.java
+++ smart-generator-plugin-model/src/smart/generator/plugin/model/readers/IAnnotationReader.java
@@ -2,12 +2,12 @@ package smart.generator.plugin.model.readers;
 
 import org.eclipse.jdt.core.dom.MemberValuePair;
 
-import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 
 public interface IAnnotationReader<T> {
-	public String name(T annotation);
+	public String getName(T annotation);
 
-	public ArrayListMultimap<String, String> getValue(T annotation);
+	public Multimap<String, String> getValue(T annotation);
 
-	public ArrayListMultimap<String, String> getValue(MemberValuePair pair);
+	public Multimap<String, String> getValue(MemberValuePair pair);
 }
