28745e2169c1bd3e85f6a66e3677c5a2d8fe03d6
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-06-16 15:18:34 -0300

    add de images

--- smart-generator-plugin-core-ui/src/smart/generator/plugin/core/ui/providers/ProjectLabelProvider.java
+++ smart-generator-plugin-core-ui/src/smart/generator/plugin/core/ui/providers/ProjectLabelProvider.java
@@ -2,8 +2,16 @@ package smart.generator.plugin.core.ui.providers;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.PlatformUI;
 
 public class ProjectLabelProvider extends LabelProvider {
+
+	@Override
+	public Image getImage(Object element) {
+		return PlatformUI.getWorkbench().getSharedImages().getImage(ISharedImages. IMG_OBJ_PROJECT);
+	}
 
 	@Override
 	public String getText(Object element) {
