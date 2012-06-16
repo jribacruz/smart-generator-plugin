80f8f9afbdfccaf3b141c8cb6e9274aa9f2e701e
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-06-15 21:58:48 -0300

    Preference Page

---
+++ smart-generator-plugin-core-ui/src/smart/generator/plugin/core/ui/preferences/PreferenceConstants.java
@@ -0,0 +1,10 @@
+package smart.generator.plugin.core.ui.preferences;
+
+/**
+ * Constant definitions for plug-in preferences
+ */
+public class PreferenceConstants {
+
+	public static final String P_PATH = "pathPreference";
+
+}
---
+++ smart-generator-plugin-core-ui/src/smart/generator/plugin/core/ui/preferences/PreferenceInitializer.java
@@ -0,0 +1,23 @@
+package smart.generator.plugin.core.ui.preferences;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+
+import smart.generator.plugin.core.ui.Activator;
+
+/**
+ * Class used to initialize default preference values.
+ */
+public class PreferenceInitializer extends AbstractPreferenceInitializer {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
+	 */
+	@Override
+	public void initializeDefaultPreferences() {
+		IPreferenceStore store = Activator.getDefault().getPreferenceStore();
+	}
+
+}
