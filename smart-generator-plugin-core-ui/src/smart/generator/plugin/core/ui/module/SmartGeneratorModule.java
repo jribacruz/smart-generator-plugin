f83506bba6e646d1de59f21007e9d8aa1fb6bb8e
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-06-15 21:59:04 -0300

    Smart Generator Module

---
+++ smart-generator-plugin-core-ui/src/smart/generator/plugin/core/ui/module/SmartGeneratorModule.java
@@ -0,0 +1,19 @@
+package smart.generator.plugin.core.ui.module;
+
+import smart.generator.plugin.core.ui.context.ApplicationContext;
+import smart.generator.plugin.model.manager.ModelManager;
+import smart.generator.plugin.template.manager.TemplateManager;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Scopes;
+
+public class SmartGeneratorModule extends AbstractModule {
+
+	@Override
+	protected void configure() {
+		bind(ApplicationContext.class).in(Scopes.SINGLETON);
+		bind(TemplateManager.class).in(Scopes.SINGLETON);
+		bind(ModelManager.class).in(Scopes.SINGLETON);
+	}
+
+}
