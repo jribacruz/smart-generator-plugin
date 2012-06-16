58321f32d5e31df8780e19829cca2570a9051c1e
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-06-16 15:37:49 -0300

    wizard.dispose()

--- smart-generator-plugin-core-ui/src/smart/generator/plugin/core/ui/commands/GeneratorCommand.java
+++ smart-generator-plugin-core-ui/src/smart/generator/plugin/core/ui/commands/GeneratorCommand.java
@@ -1,7 +1,9 @@
 package smart.generator.plugin.core.ui.commands;
 
+import org.apache.commons.lang.StringUtils;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
@@ -26,9 +28,15 @@ public class GeneratorCommand {
 	public void execute(ExecutionEvent event) throws ExecutionException {
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
 		context.setSelection(window.getSelectionService().getSelection());
-		WizardDialog dialog = new WizardDialog(window.getShell(), wizard);
-		log.info("Iniciando geração...");
-		dialog.create();
-		dialog.open();
+		if (!StringUtils.isEmpty(context.getRepositoryPath())) {
+			WizardDialog dialog = new WizardDialog(window.getShell(), wizard);
+			log.info("Iniciando geração...");
+			dialog.create();
+			dialog.open();
+		} else {
+			MessageDialog
+			.openWarning(window.getShell(), "Aviso", "Selecione um reporitorio. Ferramentas -> Smart Generator");
+		}
+		wizard.dispose();
 	}
 }
