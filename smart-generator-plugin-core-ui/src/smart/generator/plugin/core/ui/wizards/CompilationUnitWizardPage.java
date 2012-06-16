96ece353e9cc85d695bcb68896910cefa5542944
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-06-16 13:45:10 -0300

    CompilationUnit selecionadas

--- smart-generator-plugin-core-ui/src/smart/generator/plugin/core/ui/wizards/CompilationUnitWizardPage.java
+++ smart-generator-plugin-core-ui/src/smart/generator/plugin/core/ui/wizards/CompilationUnitWizardPage.java
@@ -1,10 +1,13 @@
 package smart.generator.plugin.core.ui.wizards;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jface.dialogs.IDialogPage;
+import org.eclipse.jface.viewers.CheckStateChangedEvent;
 import org.eclipse.jface.viewers.CheckboxTableViewer;
+import org.eclipse.jface.viewers.ICheckStateListener;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
@@ -22,11 +25,14 @@ import smart.generator.plugin.core.ui.providers.CompilationUnitLabelProvider;
 
 public class CompilationUnitWizardPage extends WizardPage {
 
+	@SuppressWarnings("unused")
 	private ISelection selection;
 
 	private CheckboxTableViewer tableViewer;
 
 	private Set<ICompilationUnit> units;
+
+	private Set<ICompilationUnit> selectedUnits;
 
 	/**
 	 * Constructor for SampleNewWizardPage.
@@ -38,6 +44,7 @@ public class CompilationUnitWizardPage extends WizardPage {
 		setTitle("Metamodelos");
 		setDescription("Selecione os metamodelos que serão usados para a geração");
 		this.selection = selection;
+		this.selectedUnits = new HashSet<ICompilationUnit>();
 	}
 
 	/**
@@ -52,6 +59,13 @@ public class CompilationUnitWizardPage extends WizardPage {
 		tableViewer = CheckboxTableViewer.newCheckList(container, SWT.BORDER);
 		tableViewer.setLabelProvider(new CompilationUnitLabelProvider());
 		tableViewer.setContentProvider(new CompilationUnitContentProvider());
+		tableViewer.addCheckStateListener(new ICheckStateListener() {
+
+			@Override
+			public void checkStateChanged(CheckStateChangedEvent event) {
+				getWizard().getContainer().updateButtons();
+			}
+		});
 
 		initialize();
 		setControl(container);
@@ -59,7 +73,20 @@ public class CompilationUnitWizardPage extends WizardPage {
 
 	@Override
 	public boolean isPageComplete() {
-		return tableViewer.getCheckedElements().length > 0;
+		return false;
+	}
+
+	@Override
+	public boolean canFlipToNextPage() {
+		if (tableViewer.getCheckedElements().length > 0) {
+			selectedUnits.clear();
+			Object[] selectedElementList = tableViewer.getCheckedElements();
+			for (Object selectedElement : selectedElementList) {
+				selectedUnits.add((ICompilationUnit) selectedElement);
+			}
+			return true;
+		}
+		return false;
 	}
 
 	private void initialize() {
@@ -68,6 +95,10 @@ public class CompilationUnitWizardPage extends WizardPage {
 
 	public void setUnits(Set<ICompilationUnit> units) {
 		this.units = units;
+	}
+
+	public Set<ICompilationUnit> getSelectedUnits() {
+		return selectedUnits;
 	}
 
 }
