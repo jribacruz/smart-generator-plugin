926d80106aa2913b2ff1a9d64ac33c179f0b2f28
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-06-15 22:01:33 -0300

    declaracao do objetos

--- smart-generator-plugin-core-ui/src/smart/generator/plugin/core/ui/module/SmartGeneratorModule.java
+++ smart-generator-plugin-core-ui/src/smart/generator/plugin/core/ui/module/SmartGeneratorModule.java
@@ -1,5 +1,6 @@
 package smart.generator.plugin.core.ui.module;
 
+import smart.generator.plugin.core.ui.commands.GeneratorCommand;
 import smart.generator.plugin.core.ui.context.ApplicationContext;
 import smart.generator.plugin.model.manager.ModelManager;
 import smart.generator.plugin.template.manager.TemplateManager;
@@ -11,6 +12,7 @@ public class SmartGeneratorModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
+		bind(GeneratorCommand.class);
 		bind(ApplicationContext.class).in(Scopes.SINGLETON);
 		bind(TemplateManager.class).in(Scopes.SINGLETON);
 		bind(ModelManager.class).in(Scopes.SINGLETON);
