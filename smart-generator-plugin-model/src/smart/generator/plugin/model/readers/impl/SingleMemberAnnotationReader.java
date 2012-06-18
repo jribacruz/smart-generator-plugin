98c95a83fe2f23566ca6697828922655292905e3
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-06-18 15:20:38 -0300

    suporte a array

--- smart-generator-plugin-model/src/smart/generator/plugin/model/readers/impl/SingleMemberAnnotationReader.java
+++ smart-generator-plugin-model/src/smart/generator/plugin/model/readers/impl/SingleMemberAnnotationReader.java
@@ -1,14 +1,22 @@
 package smart.generator.plugin.model.readers.impl;
 
-import org.eclipse.jdt.core.dom.MemberValuePair;
-import org.eclipse.jdt.core.dom.SingleMemberAnnotation;
+import java.util.Iterator;
 
+import org.eclipse.jdt.core.dom.ArrayInitializer;
+import org.eclipse.jdt.core.dom.MemberValuePair;
+import org.eclipse.jdt.core.dom.NumberLiteral;
+import org.eclipse.jdt.core.dom.SingleMemberAnnotation;
+import org.eclipse.jdt.core.dom.StringLiteral;
+
+import smart.generator.plugin.console.core.Log;
 import smart.generator.plugin.model.readers.IAnnotationReader;
 
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
 
 public class SingleMemberAnnotationReader implements IAnnotationReader<SingleMemberAnnotation> {
+
+	private Log log = new Log();
 
 	@Override
 	public String getName(SingleMemberAnnotation annotation) {
@@ -18,7 +26,22 @@ public class SingleMemberAnnotationReader implements IAnnotationReader<SingleMem
 	@Override
 	public Multimap<String, String> getValue(SingleMemberAnnotation annotation) {
 		Multimap<String, String> multimap = ArrayListMultimap.create();
-		multimap.put("value", annotation.getValue().toString());
+		if (annotation.getValue().getClass() == ArrayInitializer.class) {
+			ArrayInitializer array = (ArrayInitializer) annotation.getValue();
+			Iterator<?> iterator = array.expressions().iterator();
+			while (iterator.hasNext()) {
+				Object item = iterator.next();
+				if (item.getClass() == StringLiteral.class) {
+					StringLiteral literal = (StringLiteral) item;
+					multimap.put("value", literal.getEscapedValue());
+				} else if (item.getClass() == NumberLiteral.class) {
+					NumberLiteral literal = (NumberLiteral) item;
+					multimap.put("value", literal.getToken());
+				}
+			}
+		} else {
+			multimap.put("value", annotation.getValue().toString());
+		}
 		return multimap;
 	}
 
