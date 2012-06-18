f51a3686e59daa82fb4a4321aa7e4f943fe82471
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-06-18 11:05:59 -0300

    xtemplate e ximport

---
+++ smart-generator-plugin-model/src/smart/generator/plugin/model/metamodel/XImport.java
@@ -0,0 +1,56 @@
+package smart.generator.plugin.model.metamodel;
+
+public class XImport {
+	private String value;
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		XImport other = (XImport) obj;
+		if (value == null) {
+			if (other.value != null) {
+				return false;
+			}
+		} else if (!value.equals(other.value)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("XImport [");
+		if (value != null) {
+			builder.append("value=");
+			builder.append(value);
+		}
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
---
+++ smart-generator-plugin-model/src/smart/generator/plugin/model/metamodel/XTemplate.java
@@ -0,0 +1,57 @@
+package smart.generator.plugin.model.metamodel;
+
+
+public class XTemplate {
+	private String template;
+
+	public String getTemplate() {
+		return template;
+	}
+
+	public void setTemplate(String template) {
+		this.template = template;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((template == null) ? 0 : template.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		XTemplate other = (XTemplate) obj;
+		if (template == null) {
+			if (other.template != null) {
+				return false;
+			}
+		} else if (!template.equals(other.template)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("XTemplate [");
+		if (template != null) {
+			builder.append("template=");
+			builder.append(template);
+		}
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
