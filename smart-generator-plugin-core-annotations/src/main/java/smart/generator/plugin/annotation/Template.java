57a0091e25c580f3dcdc488ee2730f6026bd0fa9
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-06-18 09:31:20 -0300

    to value

--- smart-generator-plugin-core-annotations/src/main/java/smart/generator/plugin/annotation/Template.java
+++ smart-generator-plugin-core-annotations/src/main/java/smart/generator/plugin/annotation/Template.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(value = ElementType.TYPE)
+@Target(value = { ElementType.TYPE, ElementType.FIELD })
 public @interface Template {
-	String[] names();
+	String[] value();
 }
