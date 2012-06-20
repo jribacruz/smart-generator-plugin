2b6a0d818f914a4183731462b8ee292149aee581
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-06-20 20:38:29 -0300

    bind TemplateLoaderManager

--- smart-generator-plugin-core-ui/src/smart/generator/plugin/core/ui/module/SmartGeneratorModule.java
+++ smart-generator-plugin-core-ui/src/smart/generator/plugin/core/ui/module/SmartGeneratorModule.java
@@ -4,9 +4,8 @@ import smart.generator.plugin.console.core.Log;
 import smart.generator.plugin.core.ui.commands.GeneratorCommand;
 import smart.generator.plugin.core.ui.context.ApplicationContext;
 import smart.generator.plugin.core.ui.wizards.GeneratorWizard;
+import smart.generator.plugin.loader.manager.TemplateLoaderManager;
 import smart.generator.plugin.model.manager.ModelManager;
-import smart.generator.plugin.template.manager.TemplateManager;
-import smart.generator.plugin.template.service.FileService;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
@@ -17,11 +16,10 @@ public class SmartGeneratorModule extends AbstractModule {
 	protected void configure() {
 		bind(GeneratorCommand.class);
 		bind(ApplicationContext.class).in(Scopes.SINGLETON);
-		bind(TemplateManager.class).in(Scopes.SINGLETON);
 		bind(ModelManager.class).in(Scopes.SINGLETON);
 		bind(GeneratorWizard.class);
 		bind(Log.class);
-		bind(FileService.class);
+		bind(TemplateLoaderManager.class);
 	}
 
 }
