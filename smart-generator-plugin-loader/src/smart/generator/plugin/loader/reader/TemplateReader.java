179a661cc9ee1fe73821180811f5e07e55a92eb2
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-06-20 20:15:03 -0300

    TemplateReader

---
+++ smart-generator-plugin-loader/src/smart/generator/plugin/loader/reader/TemplateReader.java
@@ -0,0 +1,44 @@
+package smart.generator.plugin.loader.reader;
+
+import org.apache.commons.lang.StringUtils;
+
+import smart.generator.plugin.loader.model.Template;
+
+public class TemplateReader {
+	public String getTemplateName(Template template) {
+		return template.getTemplateName();
+	}
+
+	public boolean getFileAppend(Template template) {
+		return template.isFileAppend();
+	}
+
+	public boolean appendModelName(Template template) {
+		return StringUtils.isEmpty(template.getFileName());
+	}
+
+	public String getFileName(Template template) {
+		return template.getFilePreffix() + template.getFileName() + template.getFileSuffix() + "."
+				+ template.getFileExtention();
+	}
+
+	public String getFileOutput(Template template) {
+		return template.getFileOutput();
+	}
+
+	public boolean getFileUncapitalize(Template template) {
+		return template.isUncapitalize();
+	}
+
+	public String getFileExtention(Template template) {
+		return template.getFileExtention();
+	}
+
+	public String getFilePreffix(Template template) {
+		return template.getFilePreffix();
+	}
+
+	public String getFileSuffix(Template template) {
+		return template.getFileSuffix();
+	}
+}
