819fb39a6dc63f4fd6730b0c5e7fd0d0992ea2c5
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-06-22 16:55:03 -0300

    refatoracao

--- smart-generator-plugin-writer/src/smart/generator/plugin/writer/manager/TemplateWriterManager.java
+++ smart-generator-plugin-writer/src/smart/generator/plugin/writer/manager/TemplateWriterManager.java
@@ -15,14 +15,14 @@ import org.apache.velocity.exception.ParseErrorException;
 import org.apache.velocity.exception.ResourceNotFoundException;
 
 import smart.generator.plugin.console.core.Log;
+import smart.generator.plugin.model.descriptors.ModelDescriptor;
 import smart.generator.plugin.model.descriptors.TemplateDescriptor;
 import smart.generator.plugin.model.manager.ModelManager;
 import smart.generator.plugin.model.metamodel.XModel;
-import smart.generator.plugin.model.metamodel.XTemplate;
+import smart.generator.plugin.writer.readers.ModelDescriptorReader;
 import smart.generator.plugin.writer.service.FileService;
-import smart.generator.plugin.writer.utils.TemplateUtils;
 
-import com.google.common.base.Predicate;
+import com.google.common.base.Function;
 import com.google.common.collect.Collections2;
 import com.google.inject.Inject;
 
@@ -37,42 +37,70 @@ public class TemplateWriterManager {
 	@Inject
 	private FileService service;
 
+	private Map<String, String> maps;
+
+	private List<TemplateDescriptor> descriptors;
+
+	private String projectPath;
+
 	public TemplateWriterManager() {
 		super();
+		this.maps = new HashMap<String, String>();
 	}
 
-	public void write(List<TemplateDescriptor> descriptors, final String projectPath, final XModel model) {
-		log.info("Iniciando escrita: " + model.getName());
-		initContext(model);
-		Collection<TemplateDescriptor> selectedDescriptorList = Collections2.filter(descriptors,
-				new Predicate<TemplateDescriptor>() {
+	public void init(String projectPath, List<TemplateDescriptor> descriptors) {
+		this.descriptors = descriptors;
+		this.projectPath = projectPath;
+	}
+
+	public void write(final XModel model) {
+		this.initMap(model);
+		Collection<ModelDescriptor> selectedDescriptorList = this.getSelectedDescriptorList(descriptors, model);
+		this.executeWrite(selectedDescriptorList, projectPath, model);
+
+	}
+
+	private void initMap(XModel model) {
+		maps.put("entity", model.getInstanceName());
+		maps.put("pacakage", model.getPackageName());
+	}
+
+	private Collection<ModelDescriptor> getSelectedDescriptorList(List<TemplateDescriptor> descriptors, final XModel model) {
+		return Collections2.transform(descriptors, new Function<TemplateDescriptor, ModelDescriptor>() {
 			@Override
-			public boolean apply(TemplateDescriptor descriptor) {
-				return model.getTemplates().contains(new XTemplate(descriptor.getTemplateName()));
+			public ModelDescriptor apply(TemplateDescriptor templateDescriptor) {
+				ModelDescriptorReader reader = new ModelDescriptorReader(maps, templateDescriptor, model);
+				ModelDescriptor modelDescriptor = new ModelDescriptor();
+				modelDescriptor.setFileAppend(reader.isFileAppend());
+				modelDescriptor.setFileName(reader.getFileName());
+				modelDescriptor.setFileOutput(reader.getFileOutput());
+				modelDescriptor.setTemplateName(reader.getTemplateName());
+				return modelDescriptor;
 			}
 		});
+	}
 
-		log.info("Total de template para modelo: " + model.getName() + " " + selectedDescriptorList.size());
-		log.info("Templates para modelo: " + model.getName() + " : " + selectedDescriptorList);
-
-		CollectionUtils.forAllDo(selectedDescriptorList, new Closure() {
-
+	/**
+	 * Executa a operação de escrita
+	 * 
+	 * @param modelDescriptorList
+	 * @param projectPath
+	 * @param model
+	 */
+	private void executeWrite(Collection<ModelDescriptor> modelDescriptorList, final String projectPath, final XModel model) {
+		CollectionUtils.forAllDo(modelDescriptorList, new Closure() {
 			@Override
 			public void execute(Object descriptorItem) {
-				TemplateDescriptor descriptor = (TemplateDescriptor) descriptorItem;
-				descriptor.setFileOutput(substitutorDescriptor(model, descriptor));
-				if (descriptor.isAppendModelName()) {
-					descriptor.setFileName(model.getName() + descriptor.getFileName());
-				}
-				String data = substitutorData(model, merge(model, descriptor));
-				log.info("Path do Projeto: " + projectPath);
-				service.write(projectPath, descriptor, data.getBytes());
+				ModelDescriptor descriptor = (ModelDescriptor) descriptorItem;
+				byte[] data = merge(model, descriptor);
+				service.write(projectPath, descriptor, data);
 			}
 		});
-
 	}
 
 	private VelocityContext initContext(XModel model) {
+		log.info("");
+		log.info("Inicializando contexto velocity para " + model.getName());
 		VelocityContext context = new VelocityContext();
 		context.put("app", manager.getModels());
 		context.put("model", model);
@@ -82,21 +110,7 @@ public class TemplateWriterManager {
 		return context;
 	}
 
-	private String substitutorDescriptor(XModel model, TemplateDescriptor descriptor) {
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("PACKAGE_DIR", model.getPackageName());
-		map.put("package", model.getPackageName());
-		map.put("entity", model.getName().toLowerCase());
-		return TemplateUtils.substitutor(descriptor.getFileOutput(), map);
-	}
-
-	private String substitutorData(XModel model, String data) {
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("PACKAGE", model.getAnnotation("Package").getString("name"));
-		return TemplateUtils.substitutor(data, map);
-	}
-
-	private String merge(XModel model, TemplateDescriptor descriptor) {
+	private byte[] merge(XModel model, ModelDescriptor descriptor) {
 		VelocityContext context = initContext(model);
 		StringWriter writer = new StringWriter();
 		try {
@@ -104,14 +118,12 @@ public class TemplateWriterManager {
 			template.merge(context, writer);
 		} catch (ResourceNotFoundException e) {
 			log.error("Recurso não existe: " + e.getMessage());
-			e.printStackTrace();
 		} catch (ParseErrorException e) {
 			log.error("Erro de parser: " + e.getTemplateName() + " linha: " + e.getLineNumber() + " coluna: "
 					+ e.getColumnNumber());
-			e.printStackTrace();
 		} catch (Exception e) {
-			e.printStackTrace();
+			log.error(e.getMessage());
 		}
-		return this.substitutorData(model, writer.toString());
+		return writer.toString().getBytes();
 	}
 }
